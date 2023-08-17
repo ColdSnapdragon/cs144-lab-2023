6bf8bda3ca3e1d7ed42180ff79c36f1cfbbb3a76
wgx666 <wgx.sysu@gmail.com>
2023-08-17 22:20:00 +0800

    lab 2: pass wrap and unwrap

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -5,15 +5,17 @@ using namespace std;
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
   // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return zero_point + static_cast<uint32_t>(n);
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  uint32_t diff = raw_value_ - zero_point.raw_value_;
+  uint32_t ckp_mod = static_cast<uint32_t>(checkpoint);
+  uint32_t add = diff - ckp_mod, subtract = ckp_mod - diff;
+  uint64_t added = checkpoint + add, subtracted = checkpoint - subtract;
+  if (subtracted > checkpoint || add <= subtract)
+    return added;
+  return subtracted;
 }
